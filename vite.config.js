a8642bcf5c3bf7ced8b3f110be7b55327ffe9f17
iqbaluddin <miqbaluddin@gmail.com>
2025-12-26 08:26:47 +0700

    Initial commit 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mildinbisnis",
+  base: "/mildinbisnis/",
   plugins: [react(), tailwindcss()],
 });
